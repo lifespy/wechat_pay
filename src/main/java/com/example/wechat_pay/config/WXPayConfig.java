41025d44b923ad7197ea786248ddea748d1fa867
lifespy <lifespy@qq.com>
2019-06-26 15:31:37 +0800

    init

--- src/main/java/com/example/wechat_pay/config/WXPayConfig.java
+++ src/main/java/com/example/wechat_pay/config/WXPayConfig.java
@@ -2,19 +2,33 @@ package com.example.wechat_pay.config;
 
 
 import com.github.binarywang.wxpay.config.WxPayConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WXPayConfig {
+
+    @Value("${pay.appid}")
+    private String appid;
+    @Value("${pay.mchid}")
+    private String mchid;
+    @Value("${pay.mchkey}")
+    private String mchkey;
+    @Value("${pay.submchid}")
+    private String submchid;
+    @Value("${pay.notifyurl}")
+    private String notifyurl;
+
     @Bean
     public WxPayConfig getConfig() {
         WxPayConfig payConfig = new WxPayConfig();
-        payConfig.setAppId("");
-        payConfig.setMchId("");
-        payConfig.setMchKey("");
-        payConfig.setSubMchId("");
-        payConfig.setNotifyUrl("");
+
+        payConfig.setAppId(appid);
+        payConfig.setMchId(mchid);
+        payConfig.setMchKey(mchkey);
+        payConfig.setSubMchId(submchid);
+        payConfig.setNotifyUrl(notifyurl);
         payConfig.setTradeType("NATIVE");
         return payConfig;
     }
